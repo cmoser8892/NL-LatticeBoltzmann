15eb1276678bfbc1bbd6e4f61dc25edce4ac92ed
Christoph Moser <christoph.mos.studium@gmail.com>
2022-12-13 16:35:28 +0100

    added new classes acording to the new initilization

--- src/node_generator.h
+++ src/node_generator.h
@@ -8,6 +8,7 @@
  * takes the boundary points as an ingredient
  */
 #include "types.h"
+#include "boundary_point_generator.h"
 
 typedef struct toLinks {
     int channel;
@@ -22,11 +23,22 @@ typedef struct nodePoint {
     boundaryType_t boundary; // ignored if node type is wet
 }nodePoint_t;
 
+/**
+ * In the CG class we discussed memory arrangements for NL data,
+ * a sort of Z shape seems most appropriate at least try to order them close in memory
+ */
 class node_generator {
   private:
-    // todo here
+    bool redo = false;
+    void write_data_to_file();
+    bool read_data_from_file();
+    void read_back_switch_case();
+    int determine_correct_channel();
+    void determine_neighbors();
   public:
     std::vector<nodePoint_t*> node_infos;
+    explicit node_generator(boundaryPointConstructor* p);
+    void init();
 };
 
 #endif // NL_LATTICEBOLTZMANN_NODE_GENERATOR_H
