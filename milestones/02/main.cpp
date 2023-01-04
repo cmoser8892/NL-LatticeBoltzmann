99a26e0fd1d5d44c6c49366da8f2c407896d5dcc
Christoph Moser <christoph.mos.studium@gmail.com>
2023-01-04 17:28:16 +0100

    probable pof is the stark loss of density in each step (going to 0) and the div by it

--- milestones/02/main.cpp
+++ milestones/02/main.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char *argv[]) {
     int size = 52;
-    int steps = 10000;
+    int steps = 10;
     point_t p = {size,size};
     boundaryPointConstructor boundaries(p);
     boundaries.init_sliding_lid();
@@ -16,6 +16,7 @@ int main(int argc, char *argv[]) {
         if(i % 1000 == 0) {
             std::cout << "Step: " << i << std::endl;
         }
+        sim.get_data(false);
     }
     sim.get_data(true);
     return 0;
