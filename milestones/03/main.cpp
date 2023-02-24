960b74976250afd913a2d46b22eec3c043584514
Christoph Moser <christoph.mos.studium@gmail.com>
2023-02-24 17:24:43 +0100

    Todos/Findings: looked through the big callgrids, its quite insane how much collision takes calculate seems superior than the lookup thou, prob comes down to cpu stalls, should also do a cache hit analysis prob to shed some light on that breakdown of rho seems to be systematic and not related to my code could reproduce it also in the older python code, so i will just accept it, my inti thinks its something with momentums in the moving bb todos: make a boundary for pressure variance and start testing with an object in a fluid make wierd channels to should be m4 for me -> next step

--- milestones/03/main.cpp
+++ milestones/03/main.cpp
@@ -4,14 +4,14 @@
 
 int main(int argc, char *argv[]) {
     auto start = std::chrono::high_resolution_clock::now();
-    int steps = 1000;
-    unsigned int size = 52;
-    unsigned int sub_size = 42;
+    int steps = 10000;
+    unsigned int size = 302;
+    unsigned int sub_size = 202;
     point_t c = {size,size};
-    point_t p = {sub_size,sub_size+2};
+    point_t p = {sub_size,sub_size+20};
     boundaryPointConstructor boundaries(p);
     // boundaries.init_sliding_lid_side_chopped({20,10},30);
-    boundaries.init_sliding_lid_inner({1,2},{3,4},{4,5});
+    boundaries.init_sliding_lid_inner({10,20},{34,45},{49,52});
     nodeGenerator gen(&boundaries);
     gen.init(size);
     // init sim parameters
@@ -20,7 +20,7 @@ int main(int argc, char *argv[]) {
     simulation_parameters params;
     params.u_wall = 0.1;
     params.relaxation = (2*re)/(6*base_length*params.u_wall+re);
-    params.bypass_lookup = false;
+    params.bypass_lookup = true;
     simulation sim(&boundaries,&gen);
     sim.set_simulation_parameters(params);
     sim.init();
