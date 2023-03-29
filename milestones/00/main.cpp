b9a2c6bda0ffc4489d429623fa799a9913497980
Christoph Moser <christoph.mos.studium@gmail.com>
2023-03-29 19:16:09 +0200

    Todos/Findings: true one step for nodes reduce data footprint for nodes -> toLinks to handles with some shifting -> reduce data footprint ##### todos long term: make a boundary for pressure variance and start testing with an object in a fluid investigate the separation of data in multiple points -> separation of wet and dry nodes -> separation of data-array form the nodes -> new class for the macro values rework toLinks, is mostly in order needs more testing thou make wierd channels to should be m4 for me -> next step calculate seems superior than the lookup thou, prob comes down to cpu stalls, should also do a cache hit analysis prob to shed some light on that -> still test out with more tables prob needs a SoA structure to work porperlly thou so more a bigger thing than a smaller... ##### Actual: image to boundary converter start

--- milestones/00/main.cpp
+++ milestones/00/main.cpp
@@ -1,50 +1,17 @@
-#include "simulation.h"
-#include <iostream>
-#include <chrono>
+#include <cstdint>
 
+typedef struct __attribute__((packed))
+{
+    uint16_t wFormatTag;
+    uint16_t nChannels;
+    uint32_t nSamplesPerSec;
+    uint32_t nAvgBytesPerSec;
+    uint16_t nBlockAlign;
+}
+WAVEFORMAT;
 
-int main(int argc, char *argv[]) {
-    auto start = std::chrono::high_resolution_clock::now();
-    // init variant
-    int steps = 100;
-    unsigned int size = 302;
-    point_t c = {size,size};
-    boundaryPointConstructor boundaries(c);
-    // boundaries.init_sliding_lid_side_chopped({20,10},30);
-    boundaries.init_sliding_lid();
-    nodeGenerator gen(&boundaries);
-    gen.init_fused(size);
-    /*
-    int steps = 10000;
-    unsigned int size = 302;
-    unsigned int sub_size = 202;
-    point_t c = {size,size};
-    point_t p = {sub_size,sub_size+20};
-    boundaryPointConstructor boundaries(p);
-    // boundaries.init_sliding_lid_side_chopped({20,10},30);
-    boundaries.init_sliding_lid_inner({10,20},{34,45},{49,52});
-    nodeGenerator gen(&boundaries);
-    gen.init_fused(size);
-     */
-    // init sim parameters
-    double re = 1000;
-    double base_length = size - 2;
-    simulation_parameters params;
-    params.u_wall = 0.1;
-    params.relaxation = (2*re)/(6*base_length*params.u_wall+re);
-    oSimu sim(&boundaries,&gen);
-    sim.set_simulation_parameters(params);
-    sim.init();
-    // run sim
-    for(int i = 0; i < steps; ++i) {
-        if(i % 1000 == 0) {
-            std::cout << "Step: " << i << std::endl;
-        }
-        sim.run(i);
-    }
-    sim.get_data(true,c);
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
-    std::cout << "Took " <<duration.count()<< "s" << std::endl;
-    return 0;
+int main()
+{
+    WAVEFORMAT w;
+    (void)w;
 }
--- src/image_converter.cpp
+++ src/image_converter.cpp
@@ -1,3 +1,19 @@
 #include "image_converter.h"
 
+/// todo prob best to write the converter myself lol
+imageConverter::imageConverter(std::filesystem::path p) {
+    path = p;
+}
 
+void imageConverter::check_correct_format() {
+    // checks for the bmp ending
+
+}
+
+void imageConverter::map_colours_to_boundaries() {
+
+}
+
+void imageConverter::communicate_colour_decision() {
+
+}
