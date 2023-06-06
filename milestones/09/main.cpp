31734e97f2bc4a7fa7d22f6136ce09945cb43f81
Christoph Moser <christoph.mos.studium@gmail.com>
2023-06-06 12:42:31 +0200

    Todos/Findings: method to look for the right boundary strucutre and the straights is very similar in nature ?! will try out the more popular forcing term after gua :) introduce additional pointers in the sim to remove the loading of data into memeory need to add better documentation to the whole init process latex to the rescue!?!?!? use namespaces to order code parts ?! ask andreas if this is a good idea ##### todos long term: make a boundary for pressure variance and start testing with an object in a fluid investigate the separation of data in multiple points -> separation of wet and dry nodes -> separation of data-array form the nodes -> new class for the macro values ?! ##### Actual: first groundwork for the new branch

---
+++ milestones/09/main.cpp
@@ -0,0 +1,61 @@
+#include "simulation.h"
+#include "image_converter.h"
+#include <iostream>
+#include <chrono>
+
+
+int main(int argc, char *argv[]) {
+    auto start = std::chrono::high_resolution_clock::now();
+    // init variant
+    int steps = 3000;
+    // test image setupa
+    auto bmp_24_test_image = get_base_path();
+    bmp_24_test_image.append("tests");
+    bmp_24_test_image.append("test_images");
+    bmp_24_test_image.append("small_t.bmp");
+    imageConverter ic(bmp_24_test_image);
+    // run the functions
+    ic.init();
+    ic.run();
+    // ic.boundaries->visualize_2D_boundary();
+    // ic.boundaries->visualize_2D_boundary();
+    nodeGenerator gen(ic.boundaries);
+    // if the fused init runs this test is considered complete
+    gen.init_fused(ic.return_basic_size());
+    /*
+    int steps = 10000;
+    unsigned int size = 302;
+    unsigned int sub_size = 202;
+    point_t c = {size,size};
+    point_t p = {sub_size,sub_size+20};
+    boundaryPointConstructor boundaries(p);
+    // boundaries.init_sliding_lid_side_chopped({20,10},30);
+    boundaries.init_sliding_lid_inner({10,20},{34,45},{49,52});
+    nodeGenerator gen(&boundaries);
+    gen.init_fused(size);
+     */
+    // init sim parameters
+    simulation_parameters params;
+    params.relaxation = 0.5;
+    point_t dk = {50,50};
+    goaForce rot(dk,ic.boundaries->size,-1e-3);
+    oSimu sim(ic.boundaries,&gen, &rot);
+    sim.set_simulation_parameters(params);
+    sim.init();
+    // run sim
+    for(int i = 0; i < steps; ++i) {
+        if(i % 1000 == 0) {
+            std::cout << "Step: " << i << std::endl;
+        }
+        sim.forcing_run(i);
+    }
+    // write out the data
+    sim.get_data(true);
+    if(gen.straight_surfaces != nullptr)
+        gen.straight_surfaces->write_out_surface();
+    // time the whole thing
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
+    std::cout << "Took " <<duration.count()<< "s" << std::endl;
+    return 0;
+}
---
+++ src/forced_simulation.cpp
@@ -0,0 +1,2 @@
+
+#include "forced_simulation.h"
