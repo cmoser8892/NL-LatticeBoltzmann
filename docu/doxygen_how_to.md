b6e7f1a36ed2603f73c0b9227bca364599802902
Christoph Moser <christoph.mos.studium@gmail.com>
2023-06-27 10:52:19 +0200

    Todos/Findings: method to look for the right boundary strucutre and the straights is very similar in nature ?! will try out the more popular forcing term after gua :) introduce additional pointers in the sim to remove the loading of data into memeory need to add better documentation to the whole init process latex to the rescue!?!?!? use namespaces to order code parts ?! ask andreas if this is a good idea ##### todos long term: make a boundary for pressure variance and start testing with an object in a fluid investigate the separation of data in multiple points -> separation of wet and dry nodes -> separation of data-array form the nodes -> new class for the macro values ?! ##### Actual: added a rudamentary main page

--- docu/doxygen_how_to.md
+++ docu/doxygen_how_to.md
@@ -1,4 +1,4 @@
 # Doxygen Documentation folder: 
 Call doxygen config.doxygen inf the main folder, auto generates all.
 Do not forget to install it though :)
-(Open index.html in browser to open up the documentation, is in html folder after generation)
+(Open index.html in browser to open up the documentation. The file is in html folder after generation. Or use latex up to you.)
---
+++ docu/main_page.cpp
@@ -0,0 +1,16 @@
+// note this is just cpp cause it makes clion help me write
+
+
+
+
+/** \mainpage Lattice Boltzmann with Neighborhood Lists
+ * \subsection General
+ * The project is written in C++20. The main goal is to create a datastructures
+ * that enables us to be quite flexible with boundaries in a Lattice Boltzmann Simulation.
+ * For this we use neighborhood lists to connect a bunch of loose populations together.
+ * \subsection Dependencies
+ * Eigen 3.10. \n
+ * C++ Standard Libraries
+ *
+*/
+
