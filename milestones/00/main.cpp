c25354ccfbc4af09ce263d337be9ee0a8a13d279
Christoph Moser <christoph.mos.studium@gmail.com>
2023-07-04 14:42:16 +0200

    Todos/Findings: method to look for the right boundary strucutre and the straights is very similar in nature ?! will try out the more popular forcing term after gua :) introduce additional pointers in the sim to remove the loading of data into memeory need to add better documentation to the whole init process latex to the rescue!?!?!? use namespaces to order code parts ?! ask andreas if this is a good idea ##### todos long term: make a boundary for pressure variance and start testing with an object in a fluid investigate the separation of data in multiple points -> separation of wet and dry nodes -> separation of data-array form the nodes -> new class for the macro values ?! ##### Actual: compared against python should seems simliar

--- milestones/00/main.cpp
+++ milestones/00/main.cpp
@@ -6,11 +6,19 @@ valgrind --tool=callgrind --dump-instr=yes (p)
 
 #include <iostream>
 #include "helper_functions.h"
+#include "functions.h"
 
 /**
  * Test main.
  * @return
  */
 int main(int argc, char *argv[]) {
-
+    double range = 0;
+    double delta_range = 1;
+    if(argc > 1) {
+        range = std::stod(argv[1]);
+        delta_range = std::stod(argv[2]);
+    }
+    std::cout << kernel_3(range,delta_range);
+    return 0;
 }
