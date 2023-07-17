d309262225e486043f942bbb3421d48520b94d46
Christoph Moser <christoph.mos.studium@gmail.com>
2023-07-17 18:18:06 +0200

    Todos/Findings: alternate way to introduce surface (leverage the bmp stuff) special ibm implementation needed
    #####
    todos long term:
    make a boundary for pressure variance and start testing with an object in a fluid
    investigate the separation of data in multiple points
    -> separation of wet and dry nodes
    -> separation of data-array form the nodes
    -> new class for the macro values ?!
    #####
    Actual:
    ?
    need to write tests

--- milestones/11/main.cpp
+++ milestones/11/main.cpp
@@ -1,20 +1,15 @@
-#include "simulation.h"
-#include "image_converter.h"
 #include <iostream>
 #include <chrono>
-/**
- * 10 main, imb integration, we simulate a quader of a fluid rotating, boarders are off.
- * @param argc
- * @param argv
- * @return
- */
+#include "lbm_simulation.h"
+
 int main(int argc, char *argv[]) {
     point_t starter = {13.5,13.5};
     straight_t input;
     straightGenerator sg;
-    int steps = 10;
+    int steps = 10000;
     long canvas_size = 100;
     double side_length = 69; // with a distance of 0.75 we should get 80 markers
+    double ibm_distance = 2;
     // we put in a quader
     input.point = starter;
     input.direction = {0,1};
@@ -34,25 +29,24 @@ int main(int argc, char *argv[]) {
     sg.add_surface(input);
     sg.surface_mass_center();
     nodeGenerator ng(&sg);
-    ng.init_surface(canvas_size,2);
+    ng.init_surface(canvas_size,ibm_distance);
     ng.visualize_2D_nodes();
-    std::cout << ng.node_infos.size() << std::endl;
     simulation_parameters params;
-    params.relaxation = 0.8;
+    params.relaxation = 0.5;
+    params.ibm_range = 2;
     point_t dk = {0,0};
     // max rotation is 7.5e-3
     vector_t sizes = {canvas_size,canvas_size};
-    goaForce rot(dk,sizes,1e-5);
-    forcedSimulation sim(&ng, &rot,ng.markers,sizes);
+    goaForce rot(dk,sizes,1e-3);
+    ibmSimulation sim(&ng, &rot,ng.markers,sizes);
     sim.init();
     for(int i = 0; i < steps; ++i) {
-        if (i % 1000 == 0) {
+        if(i % 1000 == 0) {
             std::cout << "Step: " << i << std::endl;
         }
-        sim.run_ibm(i);
+        sim.run(i);
     }
     sim.get_data(true);
     if(ng.straight_surfaces != nullptr)
         ng.straight_surfaces->write_out_surface();
-    return 0;
 }
