25796595a5e3dd9183c2752611d8130c1565457e
Christoph Moser <christoph.mos.studium@gmail.com>
2023-07-25 18:15:12 +0200

    Todos/Findings: alternate way to introduce surface (leverage the bmp stuff)
    #####
    todos long term:
    Pressure boundaries are a bit of a problem in LBM, why do we not use something like the pressure motor from Tareks work to enforce a pressure gradient, would that work.
    As for the dangeling of uneven exits or more than one why not use some simple interpolation techniques for that?
    Handleling of the nl is suboptimal, we pretty much build them and then throw them away and never use them
    #####
    Actual:
    tested and implemented  a bb like link on the last imb outer node so it doesnt dangle
    right now they are set to itself so there is no streaming
    need to write a test that checks the outer nodes after a while, what happens there ?!

---
+++ milestones/11b/main.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <chrono>
+#include "lbm_simulation.h"
+
+int main(int argc, char *argv[]) {
+    point_t starter = {13,13};
+    straight_t input;
+    straightGenerator sg;
+    int steps = 80000;
+    long canvas_size = 100;
+    double side_length = 60; // with a distance of 0.75 we should get 80 markers
+    kernelType_t kernel = KERNEL_C;
+    // we put in a quader
+    input.point = starter;
+    input.direction = {0,1};
+    input.max_t = side_length;
+    sg.add_surface(input);
+    input.point += input.direction * side_length;
+    input.direction = {1,0};
+    input.max_t = side_length;
+    sg.add_surface(input);
+    input.point += input.direction * side_length;
+    input.direction = {0,-1};
+    input.max_t = side_length;
+    sg.add_surface(input);
+    input.point += input.direction * side_length;
+    input.direction = {-1,0};
+    input.max_t = side_length;
+    sg.add_surface(input);
+    sg.surface_mass_center();
+    nodeGenerator ng(&sg);
+    double ibm_distance = kernel_id_to_lattice_search(kernel);
+    ng.init_surface_return(canvas_size,ibm_distance);
+    ng.visualize_2D_nodes();
+    std::cout << ng.node_infos.size() << std::endl;
+    simulation_parameters params;
+    params.relaxation = 0.5;
+    params.ibm_range = kernel_id_to_lattice_search(kernel);
+    params.kernel_in_use = kernel;
+    params.k = 1;
+    point_t dk = {0,0};
+    // max rotation is 7.5e-3
+    vector_t sizes = {canvas_size,canvas_size};
+    goaForce rot(dk,sizes,1e-3);
+    ibmSimulation sim(&ng, &rot,ng.markers,sizes);
+    sim.set_simulation_parameters(params);
+    sim.init();
+    // put the markers in the watchdog
+    rhoWatchdog dog(0.1,sizes);
+    markerWatchdog marker_watch(0.1);
+    long marker_check = 0;
+    for(auto m : sim.markers) {
+        marker_watch.init_marker(m->position);
+    }
+    // run the sim
+    for(int i = 0; i < steps; ++i) {
+        if(i % 1000 == 0) {
+            std::cout << "Step: " << i << std::endl;
+        }
+        sim.run(i);
+        for(auto m : sim.markers) {
+            if(marker_watch.check(m->position,m->handle) == true) {
+                marker_check++;
+                if(1) {
+                    std::cout << m->position.x() << ", " << m->position.y() << "//"
+                              << m->original_position.x() << ", " << m->original_position.y() << std::endl;
+                }
+            }
+        }
+        // watchdog part
+        if(1) {
+            for(auto n : sim.nodes) {
+                if(dog.check_force(n,i)) {
+                }
+            }
+        }
+    }
+
+    sim.get_data(true);
+    if(ng.straight_surfaces != nullptr)
+        ng.straight_surfaces->write_out_surface();
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+    for(auto m : sim.markers) {
+        std::cout << m->position.x() << ", " << m->position.y() << "//"
+                  << m->original_position.x() << ", " << m->original_position.y() << std::endl;
+    }
+    std::cout << marker_check << std::endl;
+}
