0230b75685db455f3d1489ef2fa51a8ce6fc9f5f
Christoph Moser <christoph.mos.studium@gmail.com>
2023-08-06 14:01:59 +0200

    Todos/Findings: correolis force integration ##### todos long term: make a boundary for pressure variance and start testing with an object in a fluid investigate the separation of data in multiple points -> separation of wet and dry nodes -> separation of data-array form the nodes -> new class for the macro values rework toLinks, is mostly in order needs more testing thou make wierd channels to should be m4 for me -> next step calculate seems superior than the lookup thou, prob comes down to cpu stalls, should also do a cache hit analysis prob to shed some light on that -> still test out with more tables prob needs a SoA structure to work porperlly thou so more a bigger thing than a smaller... # idas deleting the neighborhood list after generation seems somewhat unproductive alternatively keep it and instead of doing a xy search do a search in the form of the velocity set force based pressure boundary?! ##### sunday funsies at this point continue with the docu

---
+++ milestones/12/main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <chrono>
+#include "lbm_simulation.h"
+#include "drawn_image_surface.h"
+
+int main(int argc, char *argv[]) {
+    straight_t input;
+    int steps = 10000;
+    long canvas_size = 200;
+    kernelType_t kernel = KERNEL_C;
+    // Load the image
+    auto test_image = get_base_path();
+    test_image.append("tests");
+    test_image.append("test_images");
+    test_image.append("cool_duck.png");
+    surfaceDrawer drawer(test_image);
+    drawer.run();
+    drawer.surface_storage.surface_mass_center();
+    nodeGenerator ng(&drawer.surface_storage);
+    double ibm_distance = kernel_id_to_lattice_search(kernel);
+    /// todo main diff between 11 and 11b kept in 12
+    ng.init_surface_return(canvas_size,ibm_distance);
+    ng.visualize_2D_nodes();
+    simulation_parameters params;
+    params.relaxation = 0.5;
+    params.ibm_range = kernel_id_to_lattice_search(kernel);
+    params.kernel_in_use = kernel;
+    params.k = 1;
+    point_t dk = {0,0};
+    // max rotation is 7.5e-3
+    vector_t sizes = {canvas_size,canvas_size};
+    goaForce rot(dk,sizes,1e-3);
+    ibmSimulation sim(&ng, &rot,ng.markers,sizes);
+    sim.set_simulation_parameters(params);
+    sim.init();
+    // put the markers in the watchdog
+    rhoWatchdog dog(0.1,sizes);
+    markerWatchdog marker_watch(0.1);
+    long marker_check = 0;
+    for(auto m : sim.markers) {
+        marker_watch.init_marker(m->position);
+    }
+    // run the sim
+    for(int i = 0; i < steps; ++i) {
+        if(i % 1000 == 0) {
+            std::cout << "Step: " << i << std::endl;
+        }
+        sim.run(i);
+        for(auto m : sim.markers) {
+            if(marker_watch.check(m->position,m->handle) == true) {
+                marker_check++;
+                if(1) {
+                    std::cout << m->position.x() << ", " << m->position.y() << "//"
+                              << m->original_position.x() << ", " << m->original_position.y() << std::endl;
+                }
+            }
+        }
+        // watchdog part
+        if(1) {
+            for(auto n : sim.nodes) {
+                if(dog.check_force(n,i)) {
+                }
+            }
+        }
+    }
+
+    sim.get_data(true);
+    if(ng.straight_surfaces != nullptr)
+        ng.straight_surfaces->write_out_surface();
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+    for(auto m : sim.markers) {
+        std::cout << m->position.x() << ", " << m->position.y() << "//"
+                  << m->original_position.x() << ", " << m->original_position.y() << std::endl;
+    }
+    std::cout << marker_check << std::endl;
+}
