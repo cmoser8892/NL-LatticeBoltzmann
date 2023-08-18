74df7330ecd45e7800bd88af233257441166670c
Christoph Moser <christoph.mos.studium@gmail.com>
2023-08-18 20:02:42 +0200

    Todos/Findings: alternate way to introduce surface (leverage the bmp stuff)
    #####
    todos long term:
    Pressure boundaries are a bit of a problem in LBM, why do we not use something like the pressure motor from Tareks work to enforce a pressure gradient, would that work.
    As for the dangeling of uneven exits or more than one why not use some simple interpolation techniques for that?
    Handleling of the nl is suboptimal, we pretty much build them and then throw them away and never use them
    #####
    Actual:
    does not work the way it was intended...

--- src/node.cpp
+++ src/node.cpp
@@ -14,6 +14,7 @@ node::node(handle_t h, int dimensions, int channels, array_t pos, boundaryType_t
     if(type == NO_BOUNDARY) {
         node_type = WET;
     }
+
     else {
         node_type = DRY;
     }
